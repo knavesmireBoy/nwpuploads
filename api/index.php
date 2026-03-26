cc2f1ded969f440e9227cc1622090b06ffee0027
Andrew Sykes <andrewsykes@btinternet.com>
2026-03-26 17:38:20 +0000

    phpinfo test OK

--- api/index.php
+++ api/index.php
@@ -2,11 +2,10 @@
 
 
 phpinfo();
-/*
+
 require_once __DIR__ . '/config.php';
-include_once HELPERS;
+//include_once HELPERS;
 include_once ACCESS;
 
-var_dump(HELPERS);
-*/
+var_dump(ACCESS);
 //require_once $_SERVER['DOCUMENT_ROOT'] . '/nwp_uploads/api/includes/access.inc.php';
