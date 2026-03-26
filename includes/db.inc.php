4a0d7f9a154d5dbaecd4d513aa971f34edc9a5ed
Andrew Sykes <andrewsykes@btinternet.com>
2026-03-26 07:25:14 +0000

    connect

--- includes/db.inc.php
+++ includes/db.inc.php
@@ -1,8 +1,17 @@
 <?php
-
-
 try {
-    $pdo = new PDO('mysql:host=localhost;dbname=uploads', 'root', 'covid19krauq');
+    $params = ['host' => '127.0.0.1', 'port' => 5432, 'database' => 'uploads', 'user' => 'andrewjsykes', 'password' => 'covid19krauq'];
+    $params = ['host' => 'ep-rough-term-abqfwn54-pooler.eu-west-2.aws.neon.tech', 'port' => 5432, 'database' => 'cd_db', 'user' => 'neondb_owner', 'password' => 'npg_njmU0gaYvNV4'];
+    $db = sprintf(
+        "pgsql:host=%s;port=%d;dbname=%s;user=%s;password=%s",
+        $params['host'],
+        $params['port'],
+        $params['database'],
+        $params['user'],
+        $params['password']
+    );
+    $pdo = new PDO($db);
+   // $pdo = new PDO('mysql:host=localhost;dbname=uploads', 'root', 'covid19krauq');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->exec('SET NAMES "utf8"');
     //$conn = mysql_connect('localhost', 'root', 'krauq');
