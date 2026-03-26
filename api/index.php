8d08a63c46a4ef19eb7c78fd17335814710bc8ff
Andrew Sykes <andrewsykes@btinternet.com>
2026-03-26 17:54:51 +0000

    pOST  phpinfo2 helpers maybe

--- api/index.php
+++ api/index.php
@@ -1,8 +1,7 @@
 <?php
-
 require_once __DIR__ . '/config.php';
-//include_once HELPERS;
+include_once HELPERS;
 include_once ACCESS;
 
-var_dump(ACCESS);
+dump([22,ACCESS]);
 //require_once $_SERVER['DOCUMENT_ROOT'] . '/nwp_uploads/api/includes/access.inc.php';
