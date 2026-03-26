1db9c280633fa7ce06aafb47970fbfccba70d34c
Andrew Sykes <andrewsykes@btinternet.com>
2026-03-26 17:52:08 +0000

    pOST  phpinfo2 helpers

--- api/includes/access.inc.php
+++ api/includes/access.inc.php
@@ -1,5 +1,5 @@
 <?php
-include_once $_SERVER['DOCUMENT_ROOT'] . '/nwp_uploads/api/includes/helpers.inc.php';
+//include_once $_SERVER['DOCUMENT_ROOT'] . '/nwp_uploads/api/includes/helpers.inc.php';
 
 function databaseContainsUser($email, $password)
 {
