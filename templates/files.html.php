22dc66dafcafdcdd71a1c829584084ffeeef408e
Andrew Sykes <andrewsykes@btinternet.com>
2026-03-27 14:31:38 +0000

    v5

--- templates/files.html.php
+++ templates/files.html.php
@@ -78,7 +78,7 @@ if (isset($template)) {
 
 if (count($files) > 0): ?>
     </form>
-    <h4>The following files are stored in the database v4</h4>
+    <h4>The following files are stored in the database no middle line</h4>
     <?php
     include '_tablehead.html.php'; ?>
     <tbody>
