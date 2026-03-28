2224d0b4635f12668143aabc46002185c428f99c
Andrew Sykes <andrewsykes@btinternet.com>
2026-03-28 11:13:11 +0000

    get curval

--- api/includes/helpers.inc.php
+++ api/includes/helpers.inc.php
@@ -396,6 +396,7 @@ function lastInsert($pdo, $db = 'mysql', $tblname = '')
         $pdo->exec('SET search_path TO uploads');
         $st = doQuery($pdo, "SELECT currval(pg_get_serial_sequence('usr', 'id')) AS id", 'error obtaining last insert id');
         $row = $st->fetch(PDO::FETCH_ASSOC);
+        dump($row);
         return $row['id'];
         return $pdo->lastInsertId("{$tblname}_id_seq");
     }
