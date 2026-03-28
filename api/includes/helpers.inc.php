ab4edff3e55d9d93c48ba395f831bb19545aae94
Andrew Sykes <andrewsykes@btinternet.com>
2026-03-28 10:55:48 +0000

    get curval

--- api/includes/helpers.inc.php
+++ api/includes/helpers.inc.php
@@ -393,10 +393,9 @@ function lastInsert($pdo, $db = 'mysql', $tblname = '')
 {
     if ($db = 'postgres' && $tblname) {
         include CONNECT;
-        $st = doQuery($pdo, "SELECT currval(pg_get_serial_sequence('usr', 'id'))", 'error obtaining last insert id');
+        $st = doQuery($pdo, "SELECT currval(pg_get_serial_sequence($tblname, 'id'))", 'error obtaining last insert id');
         $row = $st->fetch(PDO::FETCH_ASSOC);
-        dump($row);
-        return $row['id'];
+        return $row['curval'];
         return $pdo->lastInsertId("{$tblname}_id_seq");
     }
     return $pdo->lastInsertId();
