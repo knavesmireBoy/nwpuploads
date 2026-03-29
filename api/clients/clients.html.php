b8bb4ade53da3ef09b196b4d32823f5bd14ec48a
Andrew Sykes <andrewsykes@btinternet.com>
2026-03-29 09:42:06 +0100

    clients

--- api/clients/clients.html.php
+++ api/clients/clients.html.php
@@ -1,7 +1,9 @@
 <?php
+/*
 require_once '../config.php';
 include_once HELPERS;
 include_once ACCESS;
+*/
 //include_once TEMPLATE . 'base.html.php';
 foreach (get_defined_vars() as $k => $v) {
     $i = 0;
