5d9699165108a367ed52c609a17960a5c71bb08a
Andrew Sykes <andrewsykes@btinternet.com>
2026-03-30 09:51:14 +0100

    env

--- api/includes/db.inc.php
+++ api/includes/db.inc.php
@@ -1,11 +1,16 @@
 <?php
 try {
+
     $env = getenv();
-    $db = $env['DATABASE_URL'];
-    preg_match('/[^:]+:([^@])+@(.)+/', $db, $matches);
-    dump($matches);
+    preg_match('/[^:]+:\/\/[^:]+:([^@]+)@(.+)/', $env['DATABASE_URL'] ?? '', $matches);
+    $pwd = $matches[1] ?? null;
+    $connect = $matches[2] ?? null;
+
+    if (!$pwd) {
+        throw new Exception('Unable to connect to the database server');
+    }
     $params = ['host' => '127.0.0.1', 'port' => 5432, 'database' => 'uploads', 'user' => 'andrewjsykes', 'password' => 'covid19krauq'];
-    $params = ['host' => 'ep-rough-term-abqfwn54-pooler.eu-west-2.aws.neon.tech', 'port' => 5432, 'database' => 'uploads', 'user' => 'neondb_owner', 'password' => 'npg_njmU0gaYvNV4'];
+    $params = ['host' => $connect, 'port' => 5432, 'database' => 'uploads', 'user' => 'neondb_owner', 'password' => $pwd];
     $db = sprintf(
         "pgsql:host=%s;port=%d;dbname=%s;user=%s;password=%s",
         $params['host'],
@@ -15,11 +20,11 @@ try {
         $params['password']
     );
     $pdo = new PDO($db);
-  //  $pdo = new PDO('mysql:host=localhost;dbname=uploads', 'root', 'covid19krauq');
+    //  $pdo = new PDO('mysql:host=localhost;dbname=uploads', 'root', 'covid19krauq');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->exec('SET search_path TO uploads');
 
-   // $pdo->exec("SET NAMES utf8");
+    // $pdo->exec("SET NAMES utf8");
     //$conn = mysql_connect('localhost', 'root', 'krauq');
 } catch (PDOException $e) {
     $output = 'Unable to connect to the database server: ' . $e->getMessage();
