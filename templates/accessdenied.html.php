48314734195dec7910663651d2c3220f6455ecfe
Andrew Sykes <andrewsykes@btinternet.com>
2026-04-01 10:18:43 +0100

    access

--- templates/accessdenied.html.php
+++ templates/accessdenied.html.php
@@ -1,13 +1,13 @@
 <?php include 'head.html.php';
-$root = $_SERVER['DOCUMENT_ROOT'] . '/nwp_uploads/api/';
+//$root = $_SERVER['DOCUMENT_ROOT'] . '/nwp_uploads/api/';
 $route = preg_match("/administrators/i", $error) ? '..' : '.';
+$route = preg_match("/administrators/i", $error) ? "../?action=logout&error=$error" : "./?action=logout&error=$error";
 ?>
 	<h1>Access Denied</h1>
 	<p><?= $error; ?></p>
 	<?php
-	header("Location: ./?action=logout&error=$error");
-	//exit();
-	include '_logout.html.php';
+	//header("Location: ./?action=logout&error=$error");
+	//include '_logout.html.php';
 	?>
 	<p><a href="<?= $route; ?>">Back</a></p>
 </body>
