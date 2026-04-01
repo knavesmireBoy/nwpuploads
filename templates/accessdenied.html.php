72f01582d0acb08408a0268cecc9269d59c8cbc9
Andrew Sykes <andrewsykes@btinternet.com>
2026-04-01 09:59:19 +0100

    access

--- templates/accessdenied.html.php
+++ templates/accessdenied.html.php
@@ -2,12 +2,12 @@
 $root = $_SERVER['DOCUMENT_ROOT'] . '/nwp_uploads/api/';
 $route = preg_match("/administrators/i", $error) ? '..' : '.';
 ?>
-	<h1>Access Denied!</h1>
+	<h1>Access Denied</h1>
 	<p><?= $error; ?></p>
 	<?php
 	header("Location: ./?action=logout&error=$error");
 	//exit();
-	//include '_logout.html.php';
+	include '_logout.html.php';
 	?>
 	<p><a href="<?= $route; ?>">Back</a></p>
 </body>
