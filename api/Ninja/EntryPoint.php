8adfce4f16f8ea019d53560ec4dc6bcca4d0aba4
Andrew Sykes <andrewsykes@btinternet.com>
2026-04-11 14:15:16 +0100

    solved

--- api/Ninja/EntryPoint.php
+++ api/Ninja/EntryPoint.php
@@ -12,7 +12,6 @@ class EntryPoint
     {
         extract($variables);
         ob_start();
-        var_dump(2,TEMPLATE);
         include  TEMPLATE . $templateFileName;
         return ob_get_clean();
     }
--- api/index.php
+++ api/index.php
@@ -14,14 +14,11 @@
     }
 
     $uri = empty($route) ? '' : implode('/', $route);
-    var_dump(4,TEMPLATE);
     $home = 'home';
     //array of "actions" which need submit adding to string for processing forms; eg assignSubmit
     $posts = ['assign', 'create', 'contact', 'edit', 'login', 'manage', 'permissions', 'register', 'retrieve', 'retire', 'unarchive', 'relocate', 'swap'];
     $pp = $pages[$route[0]] ?? '';
     $website = new \PoloAfrica\PoloAfricaWebsite($pp);
-    var_dump($website);
     $entryPoint = new \Ninja\EntryPoint($website, $posts);
-    var_dump($website, $entryPoint);
     $layoutVariables = $entryPoint->run($uri, $_SERVER['REQUEST_METHOD'], 'public', $home);
     echo $entryPoint->loadTemplate($layout, $layoutVariables);
