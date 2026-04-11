3ee54a29da71ccc504cf03ebeb8beac3cea18bba
Andrew Sykes <andrewsykes@btinternet.com>
2026-04-11 17:53:06 +0100

    login..

--- api/PoloAfrica/Controllers/Login.php
+++ api/PoloAfrica/Controllers/Login.php
@@ -5,7 +5,7 @@ namespace PoloAfrica\Controllers;
 class Login
 {
     private $authentication;
-    public function __construct(\Ninja\Authentication $authentication)
+    public function __construct(/*\Ninja\Authentication*/ $authentication)
     {
         $this->authentication = $authentication;
     }
