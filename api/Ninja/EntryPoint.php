f8fe2b04e274b17758e4591740320171666348ff
Andrew Sykes <andrewsykes@btinternet.com>
2026-04-11 10:46:50 +0100

    templates

--- api/Ninja/EntryPoint.php
+++ api/Ninja/EntryPoint.php
@@ -13,7 +13,7 @@ class EntryPoint
         extract($variables);
         ob_start();
 
-        var_dump(isDir(__DIR__ . '../../templates'));
+        var_dump(1,isDir(__DIR__ . '../../templates'));
         include  TEMPLATE . $templateFileName;
         return ob_get_clean();
     }
