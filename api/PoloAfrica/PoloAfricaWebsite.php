f311014155ed46f0ef270b72eddfc804272ac351
Andrew Sykes <andrewsykes@btinternet.com>
2026-04-13 10:36:29 +0100

    user entity

--- api/PoloAfrica/PoloAfricaWebsite.php
+++ api/PoloAfrica/PoloAfricaWebsite.php
@@ -14,6 +14,7 @@ class PoloAfricaWebsite implements Website
     private $roleTable;
     private $userRoleTable;
     private $clientTable;
+    private $uploadTable;
     private $pdo;
     private $authentication;
     private $home = '';
@@ -39,6 +40,7 @@ class PoloAfricaWebsite implements Website
         $this->userRoleTable = new DatabaseTable($this->pdo, 'userrole', 'userid');
         $this->roleTable = new DatabaseTable($this->pdo, 'role', 'id');
         $this->clientTable = new DatabaseTable($this->pdo, 'client', 'id');
+        $this->uploadTable = new DatabaseTable($this->pdo, 'upload', 'id');
         $this->userTable = new DatabaseTable($this->pdo, 'usr', 'id', '\PoloAfrica\Entity\User', [&$this->userTable, $this->userRoleTable, $this->roleTable]);
         $this->authentication = new Authentication($this->userTable, 'email', 'password');
         //$this->authentication = new \stdClass();
