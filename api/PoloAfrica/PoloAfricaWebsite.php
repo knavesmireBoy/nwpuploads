366e62828cb530145233b473a61aa293049364f5
Andrew Sykes <andrewsykes@btinternet.com>
2026-04-13 13:39:19 +0100

    upload

--- api/PoloAfrica/PoloAfricaWebsite.php
+++ api/PoloAfrica/PoloAfricaWebsite.php
@@ -109,7 +109,7 @@ class PoloAfricaWebsite implements Website
             'logger' => [$this->authentication],
             'user' => [$this->userTable],
             'bolt' => [$this->authentication],
-            'uploader' => [$this->uploadTable],
+            'uploader' => [$this->uploadTable, $this->userTable],
             'spadger' => []
         ];
 
