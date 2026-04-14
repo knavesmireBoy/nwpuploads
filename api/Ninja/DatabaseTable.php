1dfb8bc668d48aa38fdedde657b3464b72fe39d4
Andrew Sykes <andrewsykes@btinternet.com>
2026-04-14 15:24:43 +0100

    insert issue

--- api/Ninja/DatabaseTable.php
+++ api/Ninja/DatabaseTable.php
@@ -24,7 +24,7 @@ class DatabaseTable
     {
         //make sure no spaces in table name: `user `
 
-        $query = 'INSERT INTO `uploads.' . $this->table . '` (';
+        $query = 'INSERT INTO ' . $this->table . ' (';
         //$query = "INSERT INTO $tbl (";
         foreach ($values as $key => $value) {
             $query .= '`' . $key . '`,';
