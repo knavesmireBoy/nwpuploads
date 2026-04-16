98a5c3b15072dfed65491cfe6cdddedb53afd483
Andrew Sykes <andrewsykes@btinternet.com>
2026-04-16 16:22:23 +0100

    client domain

--- api/PoloAfrica/Entity/Uploader.php
+++ api/PoloAfrica/Entity/Uploader.php
@@ -53,16 +53,13 @@ class Uploader extends Entity
     {
         $res = $this->table->find('userid', $this->userid);
         $tmp = 0;
-        $max = count($res) > 1 ? '1' : '0';
+        $max = count($res);
         $res = $this->fetch('USERTABLE', 'id', $this->userid);
         if ($res['client_id']) {
             $user = $this->fetch('usertable', 'id', $this->userid);
             $client = $user->fetch('clienttable', 'id', $res['client_id']);
-            $client = ['domain' => $client->domain, 'client_name' => $client->name];
+            $client = ['domain' => $client->domain, 'clientname' => $client->name];
             $tmp = $this->getClientFiles($this->userid, true);
-            if($this->getClientFiles($this->userid, true) > 1){
-                $tmp = '1';
-            }
             $max .= $tmp;
         }
         $multi = ['multi' => $max];
