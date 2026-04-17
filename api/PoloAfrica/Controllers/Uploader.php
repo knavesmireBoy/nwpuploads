e4e1f95afd0ca1b2f390f48bfa08c107d3ef5088
Andrew Sykes <andrewsykes@btinternet.com>
2026-04-17 09:46:59 +0100

    test file exists

--- api/PoloAfrica/Controllers/Uploader.php
+++ api/PoloAfrica/Controllers/Uploader.php
@@ -118,7 +118,7 @@ class Uploader
         //$customVars: vars for prompts
         $customVars = $this->getCustomVars($key, $vars);
 
-        dump(file_exists(FILESTORE . '1776412174.jpg'));
+        dump(scandir(FILESTORE));
 
         if (isset($vars['id'])) {
             $file = $this->table->find('id', $vars['id']);
@@ -204,7 +204,7 @@ class Uploader
 
             $this->table->save($values, true);
             $key = $_POST['key'];
-            reLocate("/uploader/load/$key");
+            reLocate("/uploader/load/");
         }
     }
 
