c315fc349aa03b121bc73042d95f8d679d579343
Andrew Sykes <andrewsykes@btinternet.com>
2026-04-21 13:21:23 +0100

    search

--- templates/_logout.html.php
+++ templates/_logout.html.php
@@ -1,3 +1,3 @@
-<form action="." method="post" name="logoutform">
+<p><form action="." method="post" name="logoutform">
 <input type="hidden" name="action" value="logout" /><input type="hidden" name="goto" value="<?= $goto; ?>" /><input type="submit" value="Log out" />
-</form>
+</form></p>
