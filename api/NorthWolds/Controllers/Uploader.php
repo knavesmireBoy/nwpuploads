b868dbd308b129fa887ff8e9b112a29b55689992
Andrew Sykes <andrewsykes@btinternet.com>
2026-04-22 14:49:04 +0100

    sort tablehead

--- api/NorthWolds/Controllers/Uploader.php
+++ api/NorthWolds/Controllers/Uploader.php
@@ -159,10 +159,10 @@ class Uploader
 
             'upload' => ['template' => 'upload.html.php'],
 
-            'sort' => ['template' => 'upload.html/php'],
+            'sort' => ['template' => 'upload.html/php',
             'fhead' => $data['fhead'],
             'uhead' => $data['uhead'],
-            'thead' => $data['thead'],
+            'thead' => $data['thead']],
 
             'delete' => ['id' => $data['id'] ?? '', 'template' => 'prompt.html.php', 'title' => 'Prompt', 'prompt' => "Are you sure you want to delete this file?", 'call' => 'confirm', 'pos' => 'Yes', 'neg' => 'No', 'action' => '/uploader/confirm/'],
 
