68c37dbc034bea9a2b479997322595ef48e9dda6
Andrew Sykes <andrewsykes@btinternet.com>
2026-04-28 15:10:07 +0100

    client loaded

--- api/NorthWolds/Controllers/Client.php
+++ api/NorthWolds/Controllers/Client.php
@@ -6,8 +6,7 @@ use \Ninja\DatabaseTable;
 
 class Client
 {
-    public function __construct(private DatabaseTable $table, private DatabaseTable $usertable) {
-    }
+    public function __construct(private DatabaseTable $table, private DatabaseTable $usertable) {}
 
     private function displayer($priv)
     {
@@ -33,6 +32,7 @@ class Client
             'title' => 'Edit Clients',
             'variables' => [
                 'priv' => $priv,
+                'action' => '/client/select/',
                 'clients' => $clients
             ]
         ];
@@ -43,6 +43,44 @@ class Client
         $user = $this->usertable->find('email', $_SESSION['username'])[0];
         $details = $user->getDetails();
         $priv = $details['role'];
-        return $this->displayer($priv);
+
+        $customVars = $this->getCustomVars($key, $vars);
+
+        return $this->displayer($priv, $customVars);
+    }
+
+    private function getCustomVars($key, $data)
+    {
+        //if($key === 'confirm') dump($data);
+        $ret = [];
+        $id = $data['id'] ?? '';
+
+        $lib = [
+
+            'choose' => ['pagehead' => 'Edit Client', 'action' => '/client/edit/', 'route' => 'Edited', 'calltext' => 'Delete Client', 'callroute' => "delete=$id", 'button' => 'Update Client', 'selected' => $id],
+
+            'upload' => ['template' => 'upload.html.php', 'home' => '/uploader/load/'],
+
+            'sort' => ['fhead' => $data['fhead'] ?? '', 'uhead' => $data['uhead'] ?? '', 'thead' => $data['thead'] ?? ''],
+
+            'delete' => ['id' => $data['id'] ?? '', 'template' => 'prompt.html.php', 'title' => 'Prompt', 'prompt' => "Are you sure you want to delete this file?", 'call' => 'confirm', 'pos' => 'Yes', 'neg' => 'No', 'action' => '/uploader/confirm/'],
+
+            'confirm' => ['id' => $data['id'] ?? '', 'template' => 'prompt.html.php', 'title' => 'Prompt', 'prompt' => "Select the extent of deletions", 'delete' => 'proceed',  'action' => '/uploader/destroy/'],
+
+            'update' => ['id' => $data['id'] ?? '', 'button' =>  $data['button'] ?? '', 'all_users' => $data['users'] ?? [], 'colleagues' => $data['colleagues'] ?? [], 'group' => $data['group'] ?? '', 'answer' => $data['answer'] ?? '', 'action' => '/uploader/update/', 'template' => 'update.html.php', 'title' => 'Update', 'filename' => $data['filename'] ?? '', 'description' => $data['description'] ?? '', 'home' => '/uploader/load/']
+        ];
+
+        if ($key && isset($lib[$key])) {
+            $ret = $lib[$key];
+        }
+        return $ret;
+    }
+
+
+    public function select()
+    {
+        $client = $this->table->find('id', $_POST['client'], null, 0, 0, \PDO::FETCH_ASSOC)[0];
+        $data = ['id' => $client['id'], 'name' => $client['name'], 'domain' => $client['domain'], 'tel' => $client['tel']];
+        return $this->load('choose', $data);
     }
 }
--- templates/clients.html.php
+++ templates/clients.html.php
@@ -7,7 +7,7 @@ include TEMPLATE . '_call.html.php';
 ?>
 <?php
 if (preg_match("/admin/i", $priv)) { ?>
-	<form action="" method="post" name="clientsform" class="choose">
+	<form action="<?= $action; ?>" method="post" name="clientsform" class="choose">
 		<label for="the_client"></label>
 		<select name="client" id="the_client">
 			<option value="">Select one</option>
@@ -29,7 +29,7 @@ if (isset($template)) {
 if (isset($clientid)) { ?>
 	<p><a href="/client/load">Return to clients</a></p>
 <?php } ?>
-<p><a href="user/load/">Return to users</a></p>
+<p><a href="/user/load/">Return to users</a></p>
 
 </main>
 <footer>
