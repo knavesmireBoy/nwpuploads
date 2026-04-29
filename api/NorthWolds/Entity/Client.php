c662a76bfbc4df57b531030e82d056e2f8ae8089
Andrew Sykes <andrewsykes@btinternet.com>
2026-04-29 14:06:30 +0100

    client update validate

--- api/NorthWolds/Entity/Client.php
+++ api/NorthWolds/Entity/Client.php
@@ -9,30 +9,29 @@ class Client extends Entity
     public $domain;
     public $tel;
 
-    public function __construct(private \Ninja\DatabaseTable $table, private \Ninja\DatabaseTable $usertable)
-    {
-    }
+    public function __construct(private \Ninja\DatabaseTable $table, private \Ninja\DatabaseTable $usertable) {}
 
     public function getDetails($arg)
     {
         if (is_numeric($arg)) {
-            return $this->fetch('TABLE','id', $arg);
+            return $this->fetch('TABLE', 'id', $arg);
         } else {
             return $this->fetch('TABLE', 'domain', $arg, 'name');
         }
     }
 
-    public function foo(){
-        $users = $this->usertable->findAll(null,0,0,\PDO::FETCH_ASSOC)[0];
-        $emails = array_map(function($o) {
+    public function foo()
+    {
+        
+        
+        $users = $this->usertable->findAll(null, 0, 0, \PDO::FETCH_ASSOC)[0];
+
+            dump($users);
+        $cb = function($o) {
             $i = strrpos($o->email, '@');
-            $top = substr($o->email, $i + 1);
-            return substr($this->domain, 0, strlen($this->domain) - strlen($top) - 1);
-        }, $users);
+            return substr($o->email, $i + 1);
+        };
 
-        dump($emails);
-
-      //  $top = substr($email, $i + 1);
-       // $second = substr($this->domain, 0, strlen($this->domain) - strlen($top) - 1);
+        $d = array_map($cb, $users);
     }
 }
