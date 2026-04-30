611f4395f99048a706952a70880877240350d1a3
Andrew Sykes <andrewsykes@btinternet.com>
2026-04-30 16:21:43 +0100

    fix orderby

--- api/NorthWolds/Controllers/User.php
+++ api/NorthWolds/Controllers/User.php
@@ -12,17 +12,47 @@ class User
         return [];
     }
 
+
+    private function presentClientList($role, $prop = 'id', $flag = 'admin')
+    {
+        $users = [];
+        $client = [];
+        if (isApproved($role, $flag)) {
+            include CONNECT;
+            $st = doQuery($pdo, queryClient(null), "Error retrieving details");
+            $rows = $st->fetchAll(\PDO::FETCH_ASSOC);
+            foreach ($rows as $row) {
+                $users[$row['id']] = $row['name'];
+            }
+            $st = doQuery($pdo, "SELECT client.id, client.name, client.domain, client.tel FROM client ORDER BY name", "Database error fetching clients");
+            $rows = $st->fetchAll(\PDO::FETCH_ASSOC);
+
+            $st = $pdo->prepare("SELECT usr.id, usr.name FROM client INNER JOIN usr ON usr.client_id=client.id WHERE client.id=:id");
+            foreach ($rows as $row) {
+                $st->bindValue(":id", $row['id']);
+                doPreparedQuery($st, "Database error fetching user");
+                //filters out clients that have no users
+                if ($st->fetch(\PDO::FETCH_ASSOC)) {
+                    $client[$row[$prop]] = $row['name'];
+                }
+            }
+            return $client;
+        }
+    }
+
     private function displayer($priv, $customVars = [], $owner = [])
     {
 
-      //  $error = query();
+        //  $error = query();
         $message = $error ?? '';
-       // $pagehead_role = $nwproleplay && !obtainUserRole(true);
+        // $pagehead_role = $nwproleplay && !obtainUserRole(true);
         $predicates = [partial('preg_match', '/^nwp/')];
+        $clients = isApproved($priv, 'ADMIN') ? presentClientList($priv, 'domain') : [];
 
         $defaultVars = [
             'prompt' => null,
             'users' => [],
+            'clients' => $clients,
             'usercount' => 0,
             'denied' => false,
             'usercount' => 0,
@@ -51,7 +81,6 @@ class User
             'title' => 'Edit Users',
             'variables' => $vars
         ];
-
     }
 
     public function load(string $key = '', array $vars = [])
