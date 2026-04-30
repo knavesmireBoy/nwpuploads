701e42cf5da4fc30bb035ae6d0d6cfa22629b529
Andrew Sykes <andrewsykes@btinternet.com>
2026-04-30 12:58:58 +0100

    associate user

--- templates/associate.html.php
+++ templates/associate.html.php
@@ -1,9 +1,8 @@
-
 <form action="<?= $action; ?>" method="post" name="choice" class="prompt" id="yesno">
-<input type="hidden" name="dom" value="<?= $clientdom; ?>" />
-<input type="hidden" name="id" value="<?= $clientid; ?>" />
-<p><?= "Associate existing users with $clientname?"; ?></p>
-<label>Yes<input type="radio" name="<?= $call; ?>" value="<?= $pos; ?>" /></label>
-<label>No<input type="radio" name="<?= $call; ?>" value="<?= $neg; ?>" /></label>
-<input type="submit" value="Submit" />
+    <input type="hidden" name="dom" value="<?= $clientdom; ?>" />
+    <input type="hidden" name="id" value="<?= $clientid; ?>" />
+    <p><?= "Associate existing users with $clientname?"; ?></p>
+    <input type="radio" name="<?= $call; ?>" value="<?= $pos; ?>" id="<?= $pos; ?>" /><label for="<?= $pos; ?>">Yes</label>
+    <input type="radio" name="<?= $call; ?>" value="<?= $neg; ?>" value="<?= $neg; ?>" /><label for="<?= $neg; ?>">No</label>
+    <input type="submit" value="Submit" />
 </form>
