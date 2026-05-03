0b259430ebe2755f56f25c13328b6e6c4ddfabab
Andrew Sykes <andrewsykes@btinternet.com>
2026-05-03 09:49:56 +0100

    get client members

--- api/NorthWolds/Entity/User.php
+++ api/NorthWolds/Entity/User.php
@@ -85,15 +85,19 @@ class User extends Entity
   public function getDetails($prop = '')
   {
     $role = $this->getRole();
+    $key = 'id';
     $client = null;
     if (!empty($role)) {
-      if ($prop) {
-        return $this->{$prop};
+      if($prop === 'owner'){
+        $key = 'ownerid';
+      }
+      else if ($prop) {
+        return isset($this->{$prop}) ? $this->{$prop} : [];
       }
       if ($this->client_id) {
         $client = $this->fetch('clienttable', 'id', $this->client_id);
       }
-      return ['ownerid' => $this->id, 'name' => $this->name, 'email' => $this->email, 'role' => $role,  'client_id' => $this->client_id, 'clientname' => $client->name ?? '', 'tel' => $client->tel ?? '', 'domain' => $client->domain ?? ''];
+      return [$key => $this->id, 'name' => $this->name, 'email' => $this->email, 'role' => $role,  'client_id' => $this->client_id, 'clientname' => $client->name ?? '', 'tel' => $client->tel ?? '', 'domain' => $client->domain ?? ''];
     }
     return [];
   }
