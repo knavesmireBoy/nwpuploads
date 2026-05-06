bf8f34763ea0e5281cac7a4734550c9f9adbf20d
Andrew Sykes <andrewsykes@btinternet.com>
2026-05-06 13:42:51 +0100

    prep for messages

--- templates/userform.html.php
+++ templates/userform.html.php
@@ -15,6 +15,8 @@ if (($admin || $editor) && !empty($id)) {
 <form action="<?= $action; ?>" method="post" name="usersform" class="<?= empty($class) ? 'details' : $class; ?>">
 	<?php
 	if (isset($class) && preg_match("/override/", $class)) {
+
+		dump($_COOKIE);
 		$email = preg_match('/@/', $override) ? $override : $email;
 	?>
 		<a href="/user/load/email" class="cancel">X</a>
