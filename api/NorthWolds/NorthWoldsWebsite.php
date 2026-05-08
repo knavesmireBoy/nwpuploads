22032536b1a9d9d29bbf7cbfa9a6c5487a86d436
Andrew Sykes <andrewsykes@btinternet.com>
2026-05-08 09:31:46 +0100

    ISSUE reading tables

--- api/NorthWolds/NorthWoldsWebsite.php
+++ api/NorthWolds/NorthWoldsWebsite.php
@@ -91,9 +91,8 @@ class NorthWoldsWebsite implements Website
         $schema = function($t, $s = '') {
             return $s ? "$s.$t" : $t;
         };
-
-        $this->userRoleTable = new DatabaseTable($this->pdo, $schema('userrole'), 'userid');
         $this->roleTable = new DatabaseTable($this->pdo, $schema('role'), 'id');
+        $this->userRoleTable = new DatabaseTable($this->pdo, $schema('userrole'), 'userid');
         $this->clientTable = new DatabaseTable($this->pdo, $schema('client'), 'id', '\NorthWolds\Entity\Client', [&$this->clientTable, &$this->userTable]);
         $this->userTable = new DatabaseTable($this->pdo, $schema('usr'), 'id', '\NorthWolds\Entity\User', [&$this->userTable, $this->clientTable, $this->userRoleTable, $this->roleTable]);
         $this->uploadTable = new DatabaseTable($this->pdo, $schema('upload'), 'id', '\NorthWolds\Entity\Uploader', [&$this->uploadTable, $this->userTable]);
