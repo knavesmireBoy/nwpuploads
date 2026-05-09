d49de3b593884e83637ee8909508cc508cae78c3
Andrew Sykes <andrewsykes@btinternet.com>
2026-05-09 09:41:57 +0100

    issue on edit user

--- api/NorthWolds/Controllers/User.php
+++ api/NorthWolds/Controllers/User.php
@@ -315,7 +315,6 @@ class User extends Presenter
 
         $user = $this->table->save($data);
         $user->setRole($role); //UPDATE role here
-        dump($data);
         $updateUserDomain = $this->updateUserDomainFactory($_SESSION['role'] === 'Admin', $user, nullify($clientID), $data, $values);
         return $updateUserDomain();
         /*
