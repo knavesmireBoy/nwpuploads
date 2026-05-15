71e4ba59881fac8fb995411c6acf9c4dc44fb5d0
Andrew Sykes <andrewsykes@btinternet.com>
2026-05-15 10:23:05 +0100

    adminfooter

--- templates/adminfooter.html.php
+++ templates/adminfooter.html.php
@@ -4,27 +4,18 @@ $k = $single && ($priv === 'Client Admin') ? 'Client' : $priv;
 //could be failed attempt to edit form from a TEAM member
 //$k = !$single ? 'Client Admin' : $k;
 $routes = ['Client' => '_return2uploads.html.php', 'Client Admin' => '_return2list.html.php', 'Admin' => '_return2list.html.php'];
-/*
-if (isset($editor)) {
-	$k = $admin ? 'Admin' : $k;
-}
-else {
-	$k = 'Client';
-}
-	*/
+
 if (isset($selected)) {
 	$k = $admin ? 'Admin' : $k;
 } else {
-	$k = 'Client';
+	//$k = 'Client';
 }
 $route = $routes[$k];
-
 
 if ($admin) { ?>
 	<p class="call"><a href="/client/load/">Edit Clients</a></p>
 <?php
 }
-var_dump($k, $_SESSION['extent'], $priv);
 include $route;
 ?>
 
