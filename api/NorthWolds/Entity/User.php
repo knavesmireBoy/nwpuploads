f922d5cbc66cce9e0af6872321e420182c3e18af
Andrew Sykes <andrewsykes@btinternet.com>
2026-05-29 10:36:46 +0100

    set role

--- api/NorthWolds/Entity/User.php
+++ api/NorthWolds/Entity/User.php
@@ -143,6 +143,7 @@ class User extends Entity
       if (!empty($this->roletable->find('id', $role))) {
           $this->userroletable->save(['userid' => $this->id, 'roleid' => $role]);
       }
+      return $role;
   }
 
   private function validateDom($cid, $dbrecord, $ename, $postdom, $insertID)
