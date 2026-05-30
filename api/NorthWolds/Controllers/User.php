2ebb47f4d18a7b6fcf5b9136f09ae67dd5c60f77
Andrew Sykes <andrewsykes@btinternet.com>
2026-05-30 11:57:20 +0100

    clientAdmin role

--- api/NorthWolds/Controllers/User.php
+++ api/NorthWolds/Controllers/User.php
@@ -425,6 +425,8 @@ class User extends Presenter
         $user = $this->table->save($data);
         if(is_array($role)) dump($role[0]);
         $key = $user->setRole($role, $admin ? '_last' : 'lastadmin'); //UPDATE role here; it may trigger an error message
+
+        dump($this->home . strtolower($key));
         reLocate($this->home . strtolower($key));
     }
 
