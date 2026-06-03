d385922da3b5ac377e1892682ddad0911bb649b6
Andrew Sykes <andrewsykes@btinternet.com>
2026-06-03 18:03:09 +0100

    test add

--- api/includes/helpers.inc.php
+++ api/includes/helpers.inc.php
@@ -290,7 +290,6 @@ function doSetCookie($flag, $toggle = '')
                 unset($_COOKIE[$k]);
                 setcookie($k, '', -1, '/');
             } else {
-                dump(293);
                 setcookie($k, '', $time, '/');
                 $_COOKIE[$k] = '';
             }
