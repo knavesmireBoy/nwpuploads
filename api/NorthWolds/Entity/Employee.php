614a29d978960dc53e7e41a0720b615dcdd229a0
Andrew Sykes <andrewsykes@btinternet.com>
2026-06-04 16:34:52 +0100

    turn off cookies on fail

--- api/NorthWolds/Entity/Employee.php
+++ api/NorthWolds/Entity/Employee.php
@@ -54,7 +54,7 @@ class Employee extends User
             $cookiearg = true;
             $dbrecord = $this->fetch('TABLE', 'id', $this->id);
             $client = $this->fetch('clienttable', 'id', $cid);
-            
+            $clientdata = [];
             if (isset($dbrecord['email'])) { //existing
                 /*
                 $override is normal state ($_POST['override'] is empty) otherwise cookies have loadded new data in to the form and validateDom will fail as $postdata and $dbrecord won't tally; as this can only happen with an admin user in control we let it go
