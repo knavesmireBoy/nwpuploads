b66f27deae9474c278ac9ebc8f07807f5235fa6e
Andrew Sykes <andrewsykes@btinternet.com>
2026-06-04 16:29:47 +0100

    turn off cookies on fail

--- api/NorthWolds/Entity/Employee.php
+++ api/NorthWolds/Entity/Employee.php
@@ -54,7 +54,7 @@ class Employee extends User
             $cookiearg = true;
             $dbrecord = $this->fetch('TABLE', 'id', $this->id);
             $client = $this->fetch('clienttable', 'id', $cid);
-            $clientdata = ['client_id' => NULL];
+            
             if (isset($dbrecord['email'])) { //existing
                 /*
                 $override is normal state ($_POST['override'] is empty) otherwise cookies have loadded new data in to the form and validateDom will fail as $postdata and $dbrecord won't tally; as this can only happen with an admin user in control we let it go
@@ -65,10 +65,10 @@ class Employee extends User
                 }
                 if (!$override && $client) {
                     $clientdata = $client->validateDomain($postdata['email'], 'client_id');
-                    if(!$clientdata['client_id']){
+                    if (!$clientdata['client_id']) {
                         $relocate = "/user/load/_sync";
                         $cookiearg = false;
-                    }   
+                    }
                 }
                 $postdata = [...$postdata, ...$newdata, ...$clientdata];
 
@@ -77,15 +77,9 @@ class Employee extends User
                     $data = $this->fetch('clienttable', 'id', $cid);
                     $domain = $data->domain;
                     $postdata['email'] = "$name@$domain";
-                    if ($override) {
-                        $relocate = "/user/loadbridge/move/$uid/client_id=$cid";
-                    }
+                    $relocate = "/user/loadbridge/move/$uid/client_id=$cid";
                 } else { //admin releasing an employee OR updating name 
-
-                    if (!$cid && $override) { //empty $cd courtesy of admin
-                        $relocate = "/user/loadbridge/leave/$uid/client_id=$cid";
-                    }
-                 
+                    $relocate = !$cid && $override ? "/user/loadbridge/leave/$uid/client_id=$cid" : '';
                     $clients = $this->clienttable->findAll();
                     //allow a user to change the name part of the email address; so filter out current domain IF NOT admin
                     $f = negate(curry2('equals')("$dom.$com"));
@@ -99,16 +93,13 @@ class Employee extends User
                         $cookiearg = false;
                         reLocate("/user/load/_traitor");
                     }
-                    if(!$cid && !$override){
-                        dump([102,$postdata]);
-                    }
                 }
                 if ($relocate) {
                     $data = $cookiearg ? $postdata : $_COOKIE;
                     $this->setCookie($data, ['name', 'email', 'client_id'], $cookiearg);
                     reLocate($relocate);
                 }
-                return $postdata;
+                return $cid ? $postdata : [...$postdata, ['client_id' => null]];
             } else { //new
                 return $this->setClientEmail($cid, $name, $postdata);
             }
