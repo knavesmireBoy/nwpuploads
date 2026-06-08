9c1803b9dd5c5699db3c0978032e57d919ff05bd
Andrew Sykes <andrewsykes@btinternet.com>
2026-06-08 22:10:29 +0100

    exit

--- api/NorthWolds/Entity/Employee.php
+++ api/NorthWolds/Entity/Employee.php
@@ -81,7 +81,7 @@ class Employee extends User
                     }
                     if ($relocate) {
                         $data = $cookiearg ? $postdata : $_COOKIE;
-                        $this->setCookie([...$data, 'client_id' => 0], ['name', 'email', 'client_id'], $cookiearg);
+                        $this->setCookie([...$data, 'client_id' => ''], ['name', 'email', 'client_id'], $cookiearg);
                         reLocate($this->exit);
                     }
                     else {
