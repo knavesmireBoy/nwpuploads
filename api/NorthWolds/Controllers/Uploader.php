1de69c4ba5839434ac84102a0a3f0cefb90a127b
Andrew Sykes <andrewsykes@btinternet.com>
2026-06-10 16:22:57 +0100

    stuffedup

--- api/NorthWolds/Controllers/Uploader.php
+++ api/NorthWolds/Controllers/Uploader.php
@@ -463,6 +463,8 @@ class Uploader extends Presenter
         $this->pages = isApproved($priv, 'ADMIN') ? $this->pages : $this->setPages(count($contenders));
         //do this last; paginate
         $displayfiles = array_slice($contenders, $this->start, $this->display);
+
+        dump(467);
         return $this->displayer($user->id, $priv, $displayfiles, '', $owner, $customVars);
     }
 
