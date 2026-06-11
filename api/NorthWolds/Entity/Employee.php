902fb4df1bb155a6430440b67628d4a86ce7920e
Andrew Sykes <andrewsykes@btinternet.com>
2026-06-11 09:36:11 +0100

    fails to disaccoc

--- api/NorthWolds/Entity/Employee.php
+++ api/NorthWolds/Entity/Employee.php
@@ -51,7 +51,7 @@ class Employee extends User
         $filter = curry2('array_filter')($f);
         $clients = $this->clienttable->findAll();
 
-        $traitorCheck = composer(partial('in_array', "$edom.$ecom"), 'dump', $filter, partial('array_values'), partial('array_map', curry2('getter')(0)), partial('array_map', 'parseEmail'), partial('array_column', $clients));
+        $traitorCheck = composer(partial('in_array', $edom), $filter, partial('array_values'), partial('array_map', curry2('getter')(0)), partial('array_map', 'parseEmail'), partial('array_column', $clients));
 
         return $traitorCheck('domain');
     }
